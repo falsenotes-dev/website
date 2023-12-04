5c7a90c8c4d5dd5033c2311eebea80cd7790d7e3
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-04 20:55:10 +0900

    Update lastModified date in sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,7 +14,7 @@ export default async function Sitemap() {
     },
     ...posts.map(({ url, author, updated, updatedAt, publishedAt }) => ({
       url: `${domain}/@${author.username}/${url}`,
-      lastModified: updated ? new Date(updatedAt!) : new Date(publishedAt!),
+      lastModified: updatedAt || publishedAt || new Date(),
     })),
     ...users.map(({ username }) => ({
       url: `${domain}/@${username}`,
