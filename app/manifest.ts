53f274a0279115ad6628a8a7908ef9cb780223c1
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-04 22:50:22 +0900

    Add icons to metadata and manifest files

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,16 +11,11 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#ffffff",
     start_url: "/",
     icons: [
-      {
-        src: "/icon-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
+     {
+      src: "/apple-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+     }
     ],
   };
 }
