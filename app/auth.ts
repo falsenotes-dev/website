76ecca1bdfd0ad39932e8375519ccdf427907318
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-07 02:24:15 +0900

    Add email to user data in authentication

--- app/auth.ts
+++ app/auth.ts
@@ -98,6 +98,7 @@ export const config = {
                 },
                 data: {
                   githubId: githubId.toString(),
+                  email: email,
                 },
               })
             } catch (error) {
