43cd578026a3547b6dcbfbafbe3629767503b7a3
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-08 06:09:06 +0900

    Update SigninPage styles and add background pattern

--- app/(Auth)/signin/page.tsx
+++ app/(Auth)/signin/page.tsx
@@ -15,7 +15,7 @@ import SigninDialog from "@/components/auth/dialog"
 export default async function SigninPage() {
   return (
     <>
-      <div className="container relative h-screen w-screen">
+      <div className="container relative h-screen w-screen bg-pattern">
         <SigninDialog open />
       </div>
       {/* <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
@@ -28,8 +28,8 @@ export default async function SigninPage() {
         >
           Login
         </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
+        <div className="relative hidden h-full flex-col bg-muted p-10 border-r lg:flex ">
+          <div className="absolute inset-0 bg-pattern" />
           <Link href="/" className="relative z-20 flex items-center text-lg font-medium">
             <Icons.logo />
           </Link>
@@ -44,8 +44,8 @@ export default async function SigninPage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8 h-screen">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] h-full">
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Create an account
@@ -54,7 +54,7 @@ export default async function SigninPage() {
                 Enter your email below to create your account
               </p>
             </div>
-            
+            <UserAuthForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{" "}
               <Link
