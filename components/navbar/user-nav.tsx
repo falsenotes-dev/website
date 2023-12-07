ad4d759736cbd90d68ab97d5926098fa2804a1cd
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-07 23:16:21 +0900

    Refactor user profile display and update session user object

--- components/navbar/user-nav.tsx
+++ components/navbar/user-nav.tsx
@@ -47,12 +47,13 @@ export function UserNav() {
                          </Avatar>
                          <div className="flex flex-col space-y-1">
                               <p className="text-xl font-bold leading-none">{user.name}</p>
+                              <p className="text-sm text-muted-foreground leading-none font-normal">{user.username}</p>
                          </div>
                     </DropdownMenuLabel>
                     <div className="text-muted-foreground font-medium mt-4 mb-3.5 px-2.5">Profile</div>
                     <DropdownMenuItem asChild>
 
-                         <Link href={user.name !== null ? `/@${user.name}` : `/`} className="flex px-2.5 mb-4 py-2 border items-center">
+                         <Link href={user.name !== null ? `/@${user.username}` : `/`} className="flex px-2.5 mb-4 py-2 border items-center">
                               <Avatar className="h-6 w-6 mr-2 border">
                                    <AvatarImage src={user.image} alt={user.name} />
                                    <AvatarFallback>{user.name?.charAt(0) || user.username?.charAt(0)}</AvatarFallback>
