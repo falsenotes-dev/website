a77f2c72174a37d85202423f86e561c68a04d061
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-08 05:28:25 +0900

    Update layout.tsx to make the container width full

--- app/(Main)/[username]/[url]/layout.tsx
+++ app/(Main)/[username]/[url]/layout.tsx
@@ -230,7 +230,7 @@ export default async function PostLayout(
 
                {post && (
                     <div className="bg-popover flex flex-col gap-16 pt-16 mt-4 border-t">
-                         <div className=' md:container mx-auto px-4'>
+                         <div className=' md:container mx-auto px-4 w-full'>
                               <MoreFromAuthor post={authorPosts} author={author} sessionUser={sessionUser} />
                               {
                                    posts?.length > 0 &&
