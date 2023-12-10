8458ff7b4706f8712fec9f0ca897b4957fff0d45
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-11 02:38:03 +0900

    Update SigninPage component styles

--- app/(Auth)/signin/page.tsx
+++ app/(Auth)/signin/page.tsx
@@ -44,7 +44,7 @@ export default async function SigninPage() {
             </blockquote> */}
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="lg:p-8 md:block flex items-center min-h-screen md:min-h-fit">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
