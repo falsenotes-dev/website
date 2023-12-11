ffbfe2dd6c25492ea6a0e0e31c1dbea67ffedc52
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-12 04:18:12 +0900

    Refactor post-editor-form component

--- components/editor/post-editor-form.tsx
+++ components/editor/post-editor-form.tsx
@@ -653,7 +653,7 @@ export function PostEditorForm(props: { post: any, user: any }) {
                       Add Tag
                     </Button>
                   )}
-                  <div className="flex flex-col gap-2.5 p-4 border">
+                  <div className="flex flex-col gap-2.5">
                   <FormField
                     control={form.control}
                     name="commentsOn"
