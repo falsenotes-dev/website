5724556aa7fee2a16fda01a0f05f4d12b106d066
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-12 05:02:02 +0900

    Refactor layout and comment form components

--- app/(Main)/[username]/[url]/layout.tsx
+++ app/(Main)/[username]/[url]/layout.tsx
@@ -244,31 +244,39 @@ export default async function PostLayout({ children, params }: Props) {
   posts.length % 2 !== 0 && posts.pop();
   return (
     <>
-      <div className="md:container mx-auto px-4 pt-5">{children}</div>
+      <div
+        className="flex flex-col"
+        style={{
+          minHeight: "calc(100vh - 160px)",
+        }}
+      >
+        <div className="flex-[1_0_auto] mt-4">
+          <div className="md:container mx-auto px-4 pt-5">{children}</div>
 
-      {post && (
-        <div
-          className="bg-popover flex flex-col gap-16 pt-16 mt-4 border-t"
-          style={{
-            minHeight: "calc(100vh - 180px)",
-          }}
-        >
-          <div className="flex-[1_0_auto] md:container mx-auto px-4 w-full mb-16">
-            <MoreFromAuthor
-              post={authorPosts}
-              author={author}
-              sessionUser={sessionUser}
-            />
-            {posts?.length > 0 && (
-              <>
-                <Separator className="mt-14 mb-8" />
-                <RelatedPosts posts={posts} post={post} session={sessionUser} />
-              </>
-            )}
-          </div>
-          <SiteFooter />
+          {post && (
+            <div className="bg-popover flex flex-col gap-16 pt-16 mt-4 border-t">
+              <div className="md:container mx-auto px-4 w-full mb-16">
+                <MoreFromAuthor
+                  post={authorPosts}
+                  author={author}
+                  sessionUser={sessionUser}
+                />
+                {posts?.length > 0 && (
+                  <>
+                    <Separator className="mt-14 mb-8" />
+                    <RelatedPosts
+                      posts={posts}
+                      post={post}
+                      session={sessionUser}
+                    />
+                  </>
+                )}
+              </div>
+            </div>
+          )}
         </div>
-      )}
+      </div>
+      <SiteFooter />
     </>
   );
 }
