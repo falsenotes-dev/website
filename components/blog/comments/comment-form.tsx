fcc7e70493660dec001657fdfc59a20adeaeda0f
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-12 02:19:01 +0900

    Fix comment button disabled state

--- components/blog/comments/comment-form.tsx
+++ components/blog/comments/comment-form.tsx
@@ -127,7 +127,7 @@ export default function CommentForm(props: { post: any, session: any }) {
         <Button variant="ghost" className="flex gap-2 items-center" onClick={() => setCommenting(false)} disabled={!commenting}>
           Cancel
         </Button>
-        <Button type="submit" form={"comment"} disabled={!commenting || posting}>
+        <Button type="submit" form={"comment"} disabled={!commenting || posting || !props.post.allowComments}>
           Comment
         </Button>
       </div>
