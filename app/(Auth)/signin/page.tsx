6b474786150bfc5f77705337580a5c9e6874a392
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-12-19 01:36:57 +0900

    Update sign-in page content

--- app/(Auth)/signin/page.tsx
+++ app/(Auth)/signin/page.tsx
@@ -51,15 +51,12 @@ export default async function SigninPage({ searchParams }: { searchParams: { [ke
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+              Log in or sign up
               </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
-              </p>
             </div>
             <UserAuthForm callbackUrl={callbackUrl} />
             <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
+            By logging in or signing up using the options above, you agree to FalseNodes&apos;s{" "}
               <Link
                 href="/terms"
                 className="underline underline-offset-4 hover:text-primary"
