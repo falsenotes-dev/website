6a26ffe6fa20d697300c393a11425243e9204334
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-01-03 20:51:20 +0900

    Delete not-found.tsx files and update 404/page.tsx

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -1,21 +1,21 @@
-import { Icons } from "@/components/icon";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import Link from "next/link";
+'use client';
+import Link from "next/link"
 
-import { buttonVariants } from "@/components/ui/button";
-import { EmptyPlaceholder } from "@/components/empty-placeholder";
-import { AlertDialog, AlertDialogContent } from "@/components/ui/alert-dialog";
-import Balancer from "react-wrap-balancer";
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Icons } from "@/components/icon"
+import Balancer from "react-wrap-balancer"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils";
 
 export default function NotFound() {
+  const pathname = usePathname()
   return (
-    <div className="flex flex-col items-center justify-center bg-background gap-6 h-screen w-screen">
+    <div className="flex flex-col items-center justify-center bg-background gap-6 w-full" style={{ minHeight: 'calc(100vh - 100px)' }}>
       <Icons.errorLogo className="-mt-[90px]" />
       <h2 className="font-bold text-5xl">404</h2>
       <p className="text-muted-foreground font-light text-center">
         <Balancer>
-        Sorry 😔 — we couldn&apos;t find what you were looking for.
+          Sorry 😔 — we couldn&apos;t find what you were looking for. If you think this is a mistake, please <Link href={`mailto:supp@bkhtdev.com?subject=Error%20404:%20https://${process.env.NEXT_PUBLIC_ENV === "beta" ? 'beta.' : ''}falsenotes.dev${pathname}`} className={cn(buttonVariants({ variant: 'link' }), "text-muted-foreground font-light px-0 text-base")}>contact us</Link>.
         </Balancer>
       </p>
       <Button size={"lg"} asChild>
@@ -24,50 +24,5 @@ export default function NotFound() {
         </Link>
       </Button>
     </div>
-  );
+  )
 }
-// export default function NotFound(){
-//      const h1Style = {
-//           display: "inline-block",
-//           fontSize: "24px",
-//           fontWeight: 500,
-//         };
-
-//         const h2Style = {
-//           fontSize: "14px",
-//           fontWeight: 400,
-//           lineHeight: "28px",
-//         };
-//      return (
-//           <main className="flex max-h-screen space-y-5 flex-col items-center justify-around p-24">
-//       <div id="__next">
-//         <div style={
-//           {
-//                fontFamily:
-//                  'system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
-//                textAlign: "center",
-//                display: "flex",
-//                flexDirection: "column",
-//                alignItems: "center",
-//                height: "100vh",
-//                justifyContent: "center",
-//              }
-//         } className="space-y-10">
-//           <Icons.notFound className="h-56" />
-//           <div className="flex flex-col space-y-4">
-//             {/* 404 page title */}
-//             <h2 className="text-4xl font-bold">Lost in the Digital Abyss</h2>
-//             <Separator />
-//             {/* 404 page subtitle */}
-//             <p className="text-lg font-light">Oops! It looks like you&apos;ve taken wrong turn</p>
-//           </div>
-//           <Button variant={"outline"} size={"lg"} className="py-6" asChild>
-//             <Link href="/" className="text-lg">
-//               Go back home
-//             </Link>
-//           </Button>
-//         </div>
-//       </div>
-//     </main>
-//      )
-// }
---
+++ app/not-found.tsx
@@ -0,0 +1,28 @@
+'use client';
+import Link from "next/link"
+
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Icons } from "@/components/icon"
+import Balancer from "react-wrap-balancer"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils";
+
+export default function NotFound() {
+     const pathname = usePathname()
+     return (
+          <div className="flex flex-col items-center justify-center bg-background gap-6 w-full" style={{ minHeight: 'calc(100vh - 100px)' }}>
+               <Icons.errorLogo className="-mt-[90px]" />
+               <h2 className="font-bold text-5xl">404</h2>
+               <p className="text-muted-foreground font-light text-center">
+                    <Balancer>
+                         Sorry 😔 — we couldn&apos;t find what you were looking for. If you think this is a mistake, please <Link href={`mailto:supp@bkhtdev.com?subject=Error%20404:%20https://${process.env.NEXT_PUBLIC_ENV === "beta" ? 'beta.' : ''}falsenotes.dev${pathname}`} className={cn(buttonVariants({ variant: 'link' }), "text-muted-foreground font-light px-0 text-base")}>contact us</Link>.
+                    </Balancer>
+               </p>
+               <Button size={"lg"} asChild>
+                    <Link href="/">
+                         Go back home
+                    </Link>
+               </Button>
+          </div>
+     )
+}
