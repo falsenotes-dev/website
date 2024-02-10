7dde30572d3c376ad23525d3605e2e1375f2f897
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-02-11 02:13:58 +0900

    Refactor fetchUsers function to fetch and filter users based on followed tags and user interactions

--- components/feed/fetch-user.ts
+++ components/feed/fetch-user.ts
@@ -1,6 +1,6 @@
 import db from "@/lib/db";
 import { getSessionUser } from "../get-session-user";
-import { getFollowings } from "@/lib/prisma/session";
+import { getFollowingTags, getFollowings } from "@/lib/prisma/session";
 
 export const fetchUsers = async ({
   id,
@@ -9,6 +9,148 @@ export const fetchUsers = async ({
   id?: string | undefined;
   limit?: number | undefined;
 }) => {
+  //if id not defined, get the current user's followed tags
+  const followedTags = !id
+    ? []
+    : (await getFollowingTags({ id: id })).followingTags;
+
+  //fetch posts' authors according to the followed tags and other user who followed that tag
+  const followedTagsAuthors = await Promise.all(
+    followedTags.map((tagFollower: any) =>
+      db.user.findMany({
+        where: {
+          tagfollower: {
+            some: {
+              tagId: tagFollower.tagId,
+            },
+          },
+          id: {
+            not: id,
+          },
+        },
+        include: {
+          Followers: true,
+          Followings: true,
+          _count: {
+            select: {
+              Followers: true,
+              Followings: true,
+              publicationsPosts: {
+                where: {
+                  published: true,
+                },
+              },
+              posts: {
+                where: {
+                  published: true,
+                },
+              },
+            },
+          },
+        },
+        take: limit,
+        orderBy: [
+          {
+            Followers: {
+              _count: "desc",
+            },
+          },
+          {
+            posts: {
+              _count: "desc",
+            },
+          },
+        ],
+      })
+    )
+  );
+  const tagsPosts = await db.post.findMany({
+    where: {
+      published: true,
+      tags: {
+        some: {
+          id: {
+            in: followedTags.map((tagFollower: any) => tagFollower.tagId),
+          },
+        },
+      },
+    },
+    select: {
+      authorId: true,
+    },
+  });
+
+  const authors = await db.user.findMany({
+    where: {
+      id: {
+        in: tagsPosts.map((post) => post.authorId),
+      },
+    },
+    include: {
+      Followers: true,
+      Followings: true,
+      _count: {
+        select: {
+          Followers: true,
+          Followings: true,
+          publicationsPosts: {
+            where: {
+              published: true,
+            },
+          },
+          posts: {
+            where: {
+              published: true,
+            },
+          },
+        },
+      },
+    },
+    take: limit,
+    orderBy: [
+      {
+        Followers: {
+          _count: "desc",
+        },
+      },
+      {
+        posts: {
+          _count: "desc",
+        },
+      },
+    ],
+  });
+
+  const users = [
+    ...authors,
+    ...followedTagsAuthors.reduce((acc, val) => acc.concat(val), []),
+  ];
+
+  //remove duplicates and the current user
+  const uniqueUsers = users.filter(
+    (user, index, self) =>
+      index === self.findIndex((u) => u.id === user.id) && user.id !== id
+  );
+
+  //sort the users by followers and posts count
+  const sortedUsers = uniqueUsers.sort((a, b) => {
+    const aFollowers = a.Followers._count;
+    const bFollowers = b.Followers._count;
+    const aPosts = a._count.posts;
+    const bPosts = b._count.posts;
+    return bFollowers - aFollowers || bPosts - aPosts;
+  });
+
+  const userFollowings = id ? (await getFollowings({ id })).followings : [];
+
+  //remove the users that the current user is following
+  const filteredUsers = sortedUsers.filter(
+    (user) =>
+      !userFollowings.find(
+        (following: any) => following.following.id == user.id
+      ) && user.id !== id
+  );
+
   const topUsers = await db.user.findMany({
     include: {
       Followers: true,
@@ -57,5 +199,7 @@ export const fetchUsers = async ({
     ],
   });
 
-  return { users: JSON.parse(JSON.stringify(topUsers)) };
+  filteredUsers.length > limit && filteredUsers.splice(limit);
+
+  return { users: JSON.parse(JSON.stringify(id ? filteredUsers : topUsers)) };
 };
