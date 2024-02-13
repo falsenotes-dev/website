d3f77f2600d1fc19426790d270cd7a4edc0bf397
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-02-13 18:22:54 +0900

    Update Loading component styling

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,9 @@
 import { Icons } from "@/components/icon";
 
-export default function Loading(){
+export default function Loading() {
      return (
           <div className="w-full h-screen flex justify-center items-center bg-background">
-         <Icons.logo className="w-56 h-28 animate-pulse" />
-       </div>
+               <Icons.logo className="w-56 h-28 animate-pulse text-secondary" />
+          </div>
      )
 }
