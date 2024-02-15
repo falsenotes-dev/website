7c15862257a6f96044182193b5b00bbff3597ac6
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-02-15 18:21:30 +0900

    Refactor mode toggle component

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -19,26 +19,27 @@ import { motion } from "framer-motion"
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme()
-  const spring = {
-    type: "spring",
-    stiffness: 700,
-    damping: 30
-  };
+
+  const variants = {
+    system: { x: 0 },
+    dark: { x: '33%' },
+    light: { x: '66%' },
+  }
 
   return (
-    <RadioGroup defaultValue={theme} className="flex p-1 h-9 rounded-full bg-accent border border-input theme-switcher">
-      <motion.div className="h-full" layout transition={spring}>
+    <RadioGroup defaultValue={theme} className="flex p-1 h-9 rounded-full bg-accent border border-input theme-switcher relative overflow-hidden">
+      <div className="h-full">
         <Input type="radio" name="theme" value="system" id="r1" className="hidden" checked={theme == 'system'} onChange={() => setTheme('system')} />
         <Label htmlFor="r1" className="h-7 w-7 rounded-full flex justify-center items-center cursor-pointer"><Icons.laptop className="h-4 w-4" /></Label>
-      </motion.div>
-      <motion.div className="h-full" layout transition={spring}>
+      </div>
+      <div className="h-full">
         <Input type="radio" name="theme" value="dark" id="r2" className="hidden" checked={theme == 'dark'} onChange={() => setTheme('dark')} />
         <Label htmlFor="r2" className="h-7 w-7 rounded-full flex justify-center items-center cursor-pointer"><Icons.moon className="h-4 w-4" /></Label>
-      </motion.div>
-      <motion.div className="h-full" layout transition={spring}>
+      </div>
+      <div className="h-full">
         <Input type="radio" name="theme" id="r3" className="hidden" checked={theme == 'light'} onChange={() => setTheme('light')} />
         <Label htmlFor="r3" className="h-7 w-7 rounded-full flex justify-center items-center cursor-pointer"><Icons.sun className="h-4 w-4" /></Label>
-      </motion.div>
+      </div>
     </RadioGroup>
   )
 }
