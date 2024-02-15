b77c419b77cb0718b77d891d8d4f8a09d868f5d3
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-02-16 00:39:51 +0900

    Remove unnecessary margin class from spinner

--- components/navbar/mobile-bottom-navbar.tsx
+++ components/navbar/mobile-bottom-navbar.tsx
@@ -1,79 +1,118 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import { Icons } from "../icon"
-import { Badge } from "../ui/badge"
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
-import { useSession } from "next-auth/react"
-import { usePathname, useRouter } from "next/navigation"
-import { Button } from "../ui/button"
-import { PostCreateButton } from "./post-create-button"
+import Link from "next/link";
+import { Icons } from "../icon";
+import { Badge } from "../ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import { PostCreateButton } from "./post-create-button";
 
 export function MobileBottomNavbar() {
      const { data: session, status } = useSession() as any;
      const router = useRouter();
      const pathname = usePathname();
      return (
-          <div className="sticky py-4 border shadow-xl flex md:hidden w-full bottom-0 bg-background/60 backdrop-blur-md z-50">
+          <div className="sticky py-4 border shadow-xl flex md:hidden w-full bottom-0 bg-muted/95 backdrop-blur supports-[backdrop-filter]:bg-muted/80 z-50">
                <div className="flex items-center justify-between w-full gap-4 px-4">
                     <div className="flex items-center justify-center flex-1">
-                         <Button variant={pathname == '/feed' ? 'default' : 'ghost'} onClick={
-                              () => {
-                                   router.push('/feed')
-                              }
-                         } size={'icon'} >
+                         <Button
+                              variant={"ghost"}
+                              onClick={() => {
+                                   router.push("/feed");
+                              }}
+                              size={"icon"}
+                         >
                               <div className="flex items-center">
-                                   <Icons.home className="w-6 h-6" />
-                                   <span className="text-sm font-normal text-inherit sr-only">Home</span>
+                                   <Icons.home
+                                        className={`w-6 h-6 ${pathname == "/feed" ? "text-primary" : ""
+                                             }`}
+                                   />
+                                   <span className="text-sm font-normal text-inherit sr-only">
+                                        Home
+                                   </span>
                               </div>
                          </Button>
                     </div>
                     <div className="flex items-center justify-center flex-1">
-                         <Button variant={pathname == '/lists' ? 'default' : 'ghost'} onClick={
-                              () => {
-                                   router.push('/lists')
-                              }
-                         } size={'icon'} >
+                         <Button
+                              variant={"ghost"}
+                              onClick={() => {
+                                   router.push("/explore");
+                              }}
+                              size={"icon"}
+                         >
                               <div className="flex items-center">
-                                   <Icons.collection className="w-6 h-6" />
-                                   <span className="text-sm font-normal text-inherit sr-only">Library</span>
-                              </div>
-                         </Button>
-                    </div>
-                    <div className="flex items-center justify-center flex-1">
-                         <PostCreateButton variant={'ghost'} iconName="plusSquare" iconCLassName="h-6 w-6" />
-                    </div>
-                    <div className="flex items-center justify-center flex-1">
-                         <Button variant={pathname == '/explore' ? 'default' : 'ghost'} onClick={
-                              () => {
-                                   router.push('/explore')
-                              }
-                         } size={'icon'} >
-                              <div className="flex items-center">
-                                   <Icons.search className="w-6 h-6" />
-                                   <span className="text-sm font-normal text-inherit sr-only">Explore</span>
+                                   <Icons.search
+                                        className={`w-6 h-6 ${pathname == "/explore" ? "text-primary" : ""
+                                             }`}
+                                   />
+                                   <span className="text-sm font-normal text-inherit sr-only">
+                                        Explore
+                                   </span>
                               </div>
                          </Button>
                     </div>
 
                     <div className="flex items-center justify-center flex-1">
-                         <Button variant={'ghost'} size={'icon'} onClick={
-                              () => {
-                                   router.push(`/@${session?.user?.username}`)
-                              }
-                         } >
+                         <PostCreateButton
+                              variant={"ghost"}
+                              iconName="plusSquare"
+                              size={"icon"}
+                              iconCLassName="h-6 w-6"
+                         />
+                    </div>
+
+                    <div className="flex items-center justify-center flex-1">
+                         <Button
+                              variant={"ghost"}
+                              onClick={() => {
+                                   router.push("/lists");
+                              }}
+                              size={"icon"}
+                         >
+                              <div className="flex items-center">
+                                   <Icons.collection
+                                        className={`w-6 h-6 ${pathname == "/lists" ? "text-primary" : ""
+                                             }`}
+                                   />
+                                   <span className="text-sm font-normal text-inherit sr-only">
+                                        Library
+                                   </span>
+                              </div>
+                         </Button>
+                    </div>
+                    <div className="flex items-center justify-center flex-1">
+                         <Button
+                              variant={"ghost"}
+                              size={"icon"}
+                              onClick={() => {
+                                   router.push(`/@${session?.user?.username}`);
+                              }}
+                         >
                               <div className="flex items-center gap-1.5">
-                                   <Avatar className={`h-7 w-7 ${pathname == `/@${session?.user?.username}` ? 'border-primary border-2' : ''}`}>
-                                        <AvatarImage src={session?.user?.image || ''} alt={session?.user?.name || ''} />
+                                   <Avatar
+                                        className={`h-7 w-7 ${pathname == `/@${session?.user?.username}`
+                                             ? "border-primary border-2"
+                                             : ""
+                                             }`}
+                                   >
+                                        <AvatarImage
+                                             src={session?.user?.image || ""}
+                                             alt={session?.user?.name || ""}
+                                        />
                                         <AvatarFallback>
                                              <Icons.user className="w-6 h-6" />
                                         </AvatarFallback>
                                    </Avatar>
-                                   <span className="text-sm font-normal text-inherit sr-only">Profile</span>
+                                   <span className="text-sm font-normal text-inherit sr-only">
+                                        Profile
+                                   </span>
                               </div>
                          </Button>
                     </div>
                </div>
           </div>
-     )
+     );
 }
--- components/navbar/post-create-button.tsx
+++ components/navbar/post-create-button.tsx
@@ -69,7 +69,7 @@ export function PostCreateButton({
                {...props}
           >
                {isLoading ? (
-                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                    <Icons.spinner className="h-4 w-4 animate-spin" />
                ) : (
                     <Icon className={cn("h-[1.2rem] w-[1.2rem]", iconCLassName)} strokeWidth={1.75} />
                )}
