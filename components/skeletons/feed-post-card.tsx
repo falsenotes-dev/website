a9430b720c6443f9755e868d0b8b2d1fb4080aa2
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-03-07 21:29:35 +0900

    Update feed post card skeleton styles

--- components/skeletons/feed-post-card.tsx
+++ components/skeletons/feed-post-card.tsx
@@ -6,18 +6,18 @@ export default function PostCardSkeleton(
   { className, ...props }: React.ComponentPropsWithoutRef<typeof Card> & { className?: string; }
 ) {
   return (
-    <Card {...props} className={cn('rounded-lg feedArticleCard bg-transparent', className)}>
+    <Card {...props} className={cn('rounded-md feedArticleCard bg-transparent', className)}>
       <CardContent className="md:p-6 p-4 h-full w-full">
         <div className="flex flex-col grid-cols-12 gap-y-8 items-start h-full pb-6">
-          <div className="w-full">
-            <div className="w-full h-auto bg-muted !relative !pb-0 aspect-[3/2] rounded-lg" >
-              <Skeleton className="w-full h-full rounded-lg" />
-            </div>
-          </div>
           <div className="col-span-12 flex flex-col justify-between space-y-4 h-full w-full">
             <div className="flex items-center gap-1.5">
               <Skeleton className="w-5 h-5 rounded-full" />
               <Skeleton className="w-40 h-5" />
+            </div>
+            <div className="w-full">
+              <div className="w-full h-auto bg-muted !relative !pb-0 aspect-[3/2] rounded-lg" >
+                <Skeleton className="w-full h-full rounded-md" />
+              </div>
             </div>
             <div className="flex">
               <div className="flex-initial w-full">
