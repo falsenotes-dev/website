7c7721e4a1a87ec0b3b7c79eb4b067ec5e9c9dfe
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-03-10 03:33:45 +0900

    Add server-side rendering and mobile bottom navbar

--- app/(Main)/(feed)/layout.tsx
+++ app/(Main)/(feed)/layout.tsx
@@ -1,19 +1,24 @@
+'use server'
+import { getSessionUser } from "@/components/get-session-user"
+import { MobileBottomNavbar } from "@/components/navbar/mobile-bottom-navbar"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
      title: 'Home - FalseNotes',
 }
 
-export default function FeedLayout({
+export default async function FeedLayout({
      children,
 }: {
      children: React.ReactNode
 }) {
+     const session = await getSessionUser();
      return (
           <>
                <div className="md:flex content-center justify-center items-center flex-[0_0_auto] rounded-3xl gap-3 mb-4 h-min max-w-7xl min-w-[280px] w-full mx-auto md:mt-14 flex-col">
                     {children}
                </div>
+               <MobileBottomNavbar session={session} />
           </>
      )
 }
